5cdac925816f5dd3581650bf4604b53d01b318c4
alsidneio <alsidneio@gmail.com>
2019-01-30 15:02:26 -0800

    working to get pug file indetations correct. Styles are being applied

--- server.js
+++ server.js
@@ -4,15 +4,28 @@ const bodyParser = require('body-parser');
 const path= require('path');
 require('dotenv').config();
 
-let port =4222;
+let port =4000;
+//Grabbing all static Assets
 app.use(express.static('public'));
 
+//Load View Engine
+ app.set('view engine', 'pug');
 
+//Home Route 
 app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'));
+    res.render('index');
   });
 
   app.get('/resume', (req, res)=>{
-    res.sendFile(path.resolve(__dirname,'resume.html'));
+    res.sendFile(path.resolve(__dirname,'pages/resume.html'));
   });
+
+  app.get('/about', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/about-me.html'));
+  });
+
+  app.get('/index', (req, res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'));
+  });
+//Start Server
 app.listen(port, ()=> console.log(`Web Server started on ${port}`));
