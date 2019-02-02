8426e78141e52b098f3454ba1ed4ad8c503e236d
alsidneio <alsidneio@gmail.com>
2019-02-01 18:37:15 -0800

    adding read more read less fnctionality

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,18 @@ jQuery( document ).ready(function( $ ) {
 /*    PORTFOLIO FILTER
 /*-----------------------------------------------------------------------------------*/
 $('#Container').mixItUp();
+
+$(".more-text").hide();
+$(".read-more").on("click", function (e) {
+        e.preventDefault();
+        var txt = $(".content").is(':visible') ? 'Read More' : 'Show Less';
+        $(".read-more").text(txt);
+        $(this).('.more-text').slideToggle(200);
 });
+
+});
+
+
 
 
 
