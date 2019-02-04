40d37faf525ccddd24c85030c280d45e4b467663
alsidneio <alsidneio@gmail.com>
2019-02-04 12:04:43 -0800

    finishing resume section, Bio is done.

--- public/js/main.js
+++ public/js/main.js
@@ -10,36 +10,16 @@ $('#Container').mixItUp();
 /*-----------------------------------------------------------------------------------*/
 
 $(".complete").hide()
-$(".more-less").click(function(){
-    $(".complete").slideToggle(); 
+$(".more-less").click(function(e){
+
+    e.preventDefault();
+    $(this).prev().slideToggle(); 
         if($(this).text()=="...Show Less"){
             $(this).text("...Show More");
         }else{
             $(this).text("...Show Less");
         }      
     });
-    // let text= $(".read-more").text();
-
-    // let lessText=text.substr(0,100)
-
-    // if(text.length > 100){
-    //     $(".read-more").text(lessText);
-    //     $(".read-more").append("<a href='' class='read-more-link'> ...Show More </a>");
-    // }else{
-    //     $(".read-more").text(text);
-    // }
-
-    // $(".read-more-link").click(function(e){
-    //     e.preventDefault();
-    //     $(this).parent().text(text).append("<a href='' class='show-less-link'> ...Show Less </a>");
-    // });
-
-    // $(".show-less-link").click(function(e){
-    //     e.preventDefault();
-    //     $(this).parent().text(lessText).append("<a href='' class='read-more-link'> ...Show More</a>");
-
-    // });
-
 });
 
 
