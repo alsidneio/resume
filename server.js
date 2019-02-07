3a33605646bada782f071a0c68fe40f426592d68
alsidneio <alsidneio@gmail.com>
2019-02-06 21:15:51 -0800

    changing the npm start command in the applicaiton

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const sgMail = require('@sendgrid/mail');
 const mailer=require('./public/js/mailer');
-const Swal = require('sweetalert2');
 sgMail.setApiKey(process.env.SG_API_KEY);
 
 let port =4000;
